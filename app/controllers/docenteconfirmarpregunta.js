8bdcafb3b8899454a6042d3c024bbd9441767d51
FranVeraC <panxo_xD@outlook.cl>
2015-12-11 15:02:45 -0300

    Revert "fcd"
    This reverts commit fba3bb7accc290667538290ba22ad796d5d45fc8.

--- app/controllers/docenteconfirmarpregunta.js
+++ app/controllers/docenteconfirmarpregunta.js
@@ -1,33 +1,31 @@
 var express = require('express'),
   router = express.Router(),
   auth_docente = require("../middleware/auth_docente.js"),
-  queries = require('../queries/index.js');
-  insertar= require('../queries/insertarpregunta.js');
+  insertar = require('../queries/insertarpregunta.js');
 
-module.exports = function(app) {
-
-  var bodyParser = require('body-parser');
+module.exports = function(app){
+     var bodyParser = require('body-parser');
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
     extended: false
   }));
-  app.use('/', router);
 
-  router.get('/docente/realizar', auth_docente, function(request, response, next) {
-    response.render('docenteconfirmarpregunta', {});
-  });
-  router.post("/enviarpregunta", auth_docente, function(request, response, next) {
-    insertar.insertarpregunta.insertar_pregunta(request.body.tiempo, request.body.preMaestra, request.body.clase)
-      .then(function(insertado_pregunta) {
-        console.log("insertado:", insertado_pregunta);
-        response.redirect("/docente/realizar");
-        return;
-      })
-      .catch(function(error) {
-        console.log(error);
-        /*Como ven, acá hay dos redirect, pero no se ejecutan uno y después el otro, ya que cuando entra a un callback, el flujo de ejecución cambia
-         */
-        response.redirect("/docente/realizar");
-      })
+   router.get('/docente/realizar/:codigo/:id_pregunta', auth_docente, function(request, response, next) {
+    console.log("id usuario:",request.session.name, "tipo:", request.session.tipo);
+    response.render('docenteconfirmarpregunta', {
+      codigo:'a',
+      preguntas:['q', '2']
+    });
   })
-}
+
+
+
+
+  app.use('/', router);
+  router.post("/enviarpregunta", auth_docente,function(request, response, next){
+     console.log(request.body)
+    insertar.consultas.insertar_pregunta(time,request.params.codigo, re.params.id_pregunta);
+
+  
+   })
+};
